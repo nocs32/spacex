02c472b8f2ba86c26a970b77ca0be15d453f9053
TECHSHOP <contact.techshop@ukr.net>
2023-03-11 23:13:00 +0200

    fetched API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import SearchIcon from '@mui/icons-material/Search'
 import styled from 'styled-components'
 import Box from '@mui/material/Box'
@@ -6,10 +6,22 @@ import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
 import Select, { SelectChangeEvent } from '@mui/material/Select'
+import Tile from './components/Tile'
+import axios from 'axios'
+
+interface ILaunch {
+  date_local: string
+  success: boolean
+  links: any
+  patch: any
+  large: string
+  article: string
+}
 
 const Header = styled.header`
   display: flex;
   align-items: center;
+  justify-content: flex-end;
   padding: 16px;
 `
 const SearchBox = styled.div`
@@ -31,12 +43,51 @@ const SortHeading = styled.h4`
   font-size: 24px;
   padding: 0 30px;
 `
+const Container = styled.div`
+  max-width: 1440px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 50px 0;
+`
+const MainHeading = styled.h1`
+  font-size: 42px;
+  font-weight: 500;
+  text-align: center;
+  margin-bottom: 30px;
+`
+const TilesContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(5, 1fr);
+  justify-content: center;
+`
 
 function App() {
+  const defaultLaunches: ILaunch[] = []
+  const [launches, setLaunches]: [ILaunch[], (launches: ILaunch[]) => void] =
+    useState(defaultLaunches)
   const [date, setDate] = useState('')
   const handleChange = (event: SelectChangeEvent) => {
     setDate(event.target.value as string)
   }
+  useEffect(() => {
+    axios
+      .get<ILaunch[]>('https://api.spacexdata.com/v5/launches', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => {
+        setLaunches(response.data)
+      })
+      .catch((ex) => {
+        const error =
+          ex.response.status === 404
+            ? 'Resource Not found'
+            : 'An unexpected error has occurred'
+      })
+  }, [])
 
   return (
     <>
@@ -52,9 +103,9 @@ function App() {
               left: '16px',
             }}
           />
-          <Search />
+          <Search placeholder='Search by a launch name...' />
         </SearchBox>
-        <SortHeading>Sort by:</SortHeading>
+        <SortHeading>Filter by:</SortHeading>
         <Box sx={{ minWidth: 100 }} style={{ marginRight: '20px' }}>
           <FormControl fullWidth>
             <InputLabel>Date</InputLabel>
@@ -68,13 +119,13 @@ function App() {
               }}
             >
               <MenuItem value={'Newer'}>Newer</MenuItem>
-              <MenuItem value={'Newer'}>Older</MenuItem>
+              <MenuItem value={'Older'}>Older</MenuItem>
             </Select>
           </FormControl>
         </Box>
         <Box sx={{ minWidth: 100 }}>
           <FormControl fullWidth>
-            <InputLabel>Date</InputLabel>
+            <InputLabel>Status</InputLabel>
             <Select
               value={date}
               label='Date'
@@ -84,12 +135,25 @@ function App() {
                 height: '52px',
               }}
             >
-              <MenuItem value={'Newer'}>Newer</MenuItem>
-              <MenuItem value={'Newer'}>Older</MenuItem>
+              <MenuItem value={'Succeded'}>Succeded</MenuItem>
+              <MenuItem value={'Failed'}>Failed</MenuItem>
             </Select>
           </FormControl>
         </Box>
       </Header>
+      <Container>
+        <MainHeading>SPACEX Launches</MainHeading>
+        <TilesContainer>
+          {launches.map((launch): any => (
+            <Tile
+              imageUrl={launch.links.patch.large}
+              link={launch.links.article}
+              date={launch.date_local.split('T')[0]}
+              success={launch.success}
+            ></Tile>
+          ))}
+        </TilesContainer>
+      </Container>
     </>
   )
 }
--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,10 +1,50 @@
 import styled from 'styled-components'
 
-function Tile() {
+const TileBox = styled.a`
+  cursor: pointer;
+  max-width: 266px;
+  width: 100%;
+  height: 266px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 50px;
+  position: relative;
+`
+const TileHeading = styled.h3`
+  font-size: 24px;
+  font-weight: 700;
+  color: #fff;
+  margin-bottom: 10px;
+`
+const Status = styled.h5`
+  font-size: 30px;
+  font-weight: 700;
+`
+
+function Tile(props: {
+  imageUrl: string
+  link: string
+  date: string
+  success: boolean
+}) {
   return (
-    <>
-      <h1>Testing Tiles</h1>
-    </>
+    <TileBox
+      style={{
+        background: `no-repeat center linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${props.imageUrl}')`,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+      }}
+      href={props.link}
+      target='_blank'
+      rel='noreferrer'
+    >
+      <TileHeading>{props.date}</TileHeading>
+      <Status style={{ color: props.success ? 'green' : 'red' }}>
+        {props.success ? 'Succeded' : 'Failed'}
+      </Status>
+    </TileBox>
   )
 }
 export default Tile
